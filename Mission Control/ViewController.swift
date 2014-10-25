c26d6d11ce5cb4dff497b5f5ecd09b6a5beb9f08
Garrett Davidson <davidson.garrettm@gmail.com>
2014-10-24 21:50:48 -0400

    Initial Commit

---
+++ Mission Control/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mission Control
+//
+//  Created by Garrett Davidson on 10/24/14.
+//  Copyright (c) 2014 Garrett Davidson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
